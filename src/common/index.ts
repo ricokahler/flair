8f6973c7ff3110e14a76cfeb536d75800a36b6a7
Rico Kahler <ricokahler@me.com>
2020-02-22 17:28:40 -0500

    export css

--- src/common/index.ts
+++ src/common/index.ts
@@ -5,3 +5,4 @@ export { default as ThemeContext } from './ThemeContext';
 export { default as ThemeProvider } from './ThemeProvider';
 export { default as useColorContext } from './useColorContext';
 export { default as useTheme } from './useTheme';
+export { default as css } from './css';
