2a497c473e08bbd4a97d4bc7aab42dd2ef74a575
Rico Kahler <ricokahler@me.com>
2020-02-27 08:44:48 -0500

    update mock filename

--- __mocks__/@react-style-system/common/index.js
+++ __mocks__/@react-style-system/common/index.js
@@ -1,8 +1,16 @@
+import path from 'path';
 import * as ReactStyleSystem from '@react-style-system/common';
 
 const { createFilenameHash, ...rest } = ReactStyleSystem;
 
 module.exports = {
-  createFilenameHash: () => 'ExampleFile--00000',
+  createFilenameHash: filename => {
+    const extension = path.extname(filename);
+    const basename = path.basename(filename);
+
+    return `${path
+      .basename(filename)
+      .substring(0, basename.length - extension.length)}--00000`;
+  },
   ...rest,
 };
