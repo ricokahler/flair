291a761e6be433c821e781690923ce6bcdc87915
Rico Kahler <ricokahler@me.com>
2020-02-29 02:05:15 -0500

    test

--- packages/standalone/src/createStyles.tsx
+++ packages/standalone/src/createStyles.tsx
@@ -68,6 +68,13 @@ function createStyles<Styles extends { [key: string]: string }, Theme = any>(
 
     const incomingStyleHash = hashStyleObj(incomingStyles);
 
+    const thing = `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    
+    `
+
+    console.log(thing)
+
     // create a map of unprocessed styles
     const unprocessedStyles = useMemo(() => {
       return stylesFn({
