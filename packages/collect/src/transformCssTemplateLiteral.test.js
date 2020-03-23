f6a747366873a24ac98d0b9ee8519176f5a5c571
Rico Kahler <ricokahler@me.com>
2020-03-23 10:34:04 -0400

    add failing test

--- packages/collect/src/transformCssTemplateLiteral.test.js
+++ packages/collect/src/transformCssTemplateLiteral.test.js
@@ -2,6 +2,9 @@ import { parse } from '@babel/core';
 import { stripIndent } from 'common-tags';
 import * as t from '@babel/types';
 import generate from '@babel/generator';
+import stylis from 'stylis';
+import requireFromString from 'require-from-string';
+import prettier from 'prettier';
 import transformCssTemplateLiteral from './transformCssTemplateLiteral';
 
 it('takes in a CSS template literal and wraps non-CSS property expressions in `staticVar`', () => {
@@ -66,9 +69,152 @@ test('non-CSS property expression first', () => {
 
   const { code: outputCode } = generate(result);
   expect(outputCode).toMatchInlineSnapshot(`
+    "\`
+        \${staticVar(theme.fonts.body1)};
+        flex: 1 1 auto;
+      \`"
+  `);
+});
+
+// TODO: make this fail lol
+test.skip('calc functions', () => {
+  const code = stripIndent`\`
+    padding: \${theme.block(1)};
+    \${theme.breakpoints.down(theme.breakpoints.tablet)} {
+      padding: \${theme.block(1)};
+    }
+
+    width: 1024px;
+    max-width: 100%;
+    margin: 0 auto;
+    margin-bottom: calc(50vh - \${theme.block(2)});
+
+    & > h1 {
+      \${theme.fonts.h1};
+
+      \${theme.breakpoints.down(theme.breakpoints.tablet)} {
+        \${theme.fonts.h1};
+      }
+    }
+
+    & > h2 {
+      \${theme.fonts.h1};
+
+      \${theme.breakpoints.down(theme.breakpoints.tablet)} {
+        \${theme.fonts.h1};
+      }
+    }
+  \``;
+
+  const file = parse(code, { filename: 'example.js' });
+
+  const [firstStatement] = file.program.body;
+  const templateLiteral = firstStatement.expression;
+  expect(t.isTemplateLiteral(templateLiteral)).toBe(true);
+
+  const result = transformCssTemplateLiteral(templateLiteral);
+
+  const { code: outputCode } = generate(result);
+  expect(outputCode).toMatchInlineSnapshot(`
 "\`
-    \${staticVar(theme.fonts.body1)};
-    flex: 1 1 auto;
+    padding: \${\`\${theme.block(1)}\`};
+    \${staticVar(theme.breakpoints.down(theme.breakpoints.tablet))} {
+      padding: \${\`\${theme.block(1)}\`};
+    }
+
+    width: 1024px;
+    max-width: 100%;
+    margin: 0 auto;
+    margin-bottom: calc(50vh - \${\`\${theme.block(2)})\`};
+
+    & > h1 {
+      \${staticVar(theme.fonts.h1)};
+
+      \${staticVar(theme.breakpoints.down(theme.breakpoints.tablet))} {
+        \${staticVar(theme.fonts.h1)};
+      }
+    }
+
+    & > h2 {
+      \${staticVar(theme.fonts.h1)};
+
+      \${staticVar(theme.breakpoints.down(theme.breakpoints.tablet))} {
+        \${staticVar(theme.fonts.h1)};
+      }
+    }
   \`"
 `);
+
+  const unprocessedCss = requireFromString(stripIndent`
+    const staticVar = x => x;
+    const theme = {
+      block: () => '16px',
+      fonts: {
+        h1: 'font-weight: bold;',
+      },
+      breakpoints: {
+        tablet: '725px',
+        down: value => \`@media (max-width: \${value})\`,
+      },
+    };
+    module.exports = ${outputCode}
+  `);
+
+  stylis.set({
+    prefix: false,
+  });
+
+  const css = stylis('selector', unprocessedCss);
+
+  const prettyCss = prettier.format(css, { parser: 'css' });
+
+  expect(prettyCss).toMatchInlineSnapshot(`
+    "selector {
+      padding: 16px;
+      width: 1024px;
+      max-width: 100%;
+      margin: 0 auto;
+      margin-bottom: calc(50vh - 16px);
+    }
+    @media (max-width: 725px) {
+      selector {
+        padding: 16px;
+      }
+    }
+    selector > h1 {
+      font-weight: bold;
+    }
+    @media (max-width: 725px) {
+      selector > h1 {
+        font-weight: bold;
+      }
+    }
+    selector > h2 {
+      font-weight: bold;
+    }
+    @media (max-width: 725px) {
+      selector > h2 {
+        font-weight: bold;
+      }
+    }
+    "
+  `);
+});
+
+test('multiline CSS property', () => {
+  const code = stripIndent`\`
+    transition: background-color \${theme.durations.standard},
+      border \${theme.durations.standard};
+  \``;
+
+  const file = parse(code, { filename: 'example.js' });
+
+  const [firstStatement] = file.program.body;
+  const templateLiteral = firstStatement.expression;
+  expect(t.isTemplateLiteral(templateLiteral)).toBe(true);
+
+  const result = transformCssTemplateLiteral(templateLiteral);
+
+  const { code: outputCode } = generate(result);
+  expect(outputCode.includes('staticVar')).toBe(false);
 });
